cd99e7b32d97110205eff4a92b1151978d88e1b3
David Agustin <davidsyagustin@gmail.com>
2019-01-27 17:05:06 -0800

    Implement Win Or Lose functionality

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -4,7 +4,8 @@ export default class Game extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        currentTurn: "X",
+        currentPlayer: "X",
+        previousPlayer: "O",
         currentGameInputs: [
             [null, null, null],
             [null, null, null],
@@ -15,11 +16,19 @@ export default class Game extends React.Component {
       this.onClick = this.onClick.bind(this);
   }
 
-  onClick(y, x) {
+  componentDidMount() {
+      console.log('componentDidMount fires!')
+  }
+
+    onClick(y, x) {
       const boardManipulator = Object.values(this.state.currentGameInputs);
 
-      console.log(boardManipulator);
-      console.log('onClick fires:', y, x);
+      if (this.state.console.includes('Wins')) {
+          this.setState({
+              console: `${this.state.previousPlayer} Wins! Game has ended. Please Reset`
+          });
+          return
+      }
 
       if (boardManipulator[y][x] !== null) {
           this.setState({
@@ -27,22 +36,90 @@ export default class Game extends React.Component {
           });
           return
       }
+      boardManipulator[y][x] = this.state.currentPlayer;
 
-      boardManipulator[y][x] = this.state.currentTurn;
-
-      if (this.state.currentTurn === "X") {
+      if (this.state.currentPlayer === "X") {
           this.setState({
-              currentTurn: "O",
+              currentPlayer: "O",
+              previousPlayer: "X",
               currentGameInputs: boardManipulator,
               console: ""
-          })
+          });
+          this.gameCompletionCheck()
       } else {
           this.setState({
-              currentTurn: "X",
+              currentPlayer: "X",
+              previousPlayer: "O",
               currentGameInputs: boardManipulator,
               console: ""
-          })
+          });
+          this.gameCompletionCheck()
       }
+  }
+
+  gameCompletionCheck() {
+      const currentBoard = Object.values(this.state.currentGameInputs);
+      console.log('current player:', this.state.currentPlayer);
+      for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j ++) {
+              console.log(JSON.stringify(currentBoard[i]));
+              console.log(JSON.stringify(currentBoard[j][i]));
+              console.log(currentBoard[i].every((player) => player === this.state.currentPlayer));
+              if (currentBoard[i].every((player) => player === this.state.currentPlayer)) {
+                  this.setState({
+                      console: `${this.state.currentPlayer} Wins!`
+                  });
+              }
+          }
+      }
+      let transposedBoard = [];
+      for (let i = 0; i < currentBoard.length; ++i) {
+          for (let j = 0; j < currentBoard[i].length; ++j) {
+              // skip undefined values to preserve sparse array
+              if (currentBoard[i][j] === undefined) continue;
+              // create row if it doesn't exist yet
+              if (transposedBoard[j] === undefined) transposedBoard[j] = [];
+              // swap the x and y coords for the copy
+              transposedBoard[j][i] = currentBoard[i][j];
+          }
+      }
+
+      for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j ++) {
+              console.log(JSON.stringify(transposedBoard[i]));
+              console.log(JSON.stringify(transposedBoard[j][i]));
+              console.log(transposedBoard[i].every((player) => player === this.state.currentPlayer));
+              if (transposedBoard[i].every((player) => player === this.state.currentPlayer)) {
+                  this.setState({
+                      console: `${this.state.currentPlayer} Wins!`
+                  });
+              }
+          }
+      }
+
+      if ([currentBoard[0][0], currentBoard[1][1], currentBoard[2][2]].every((player) => player === this.state.currentPlayer)) {
+          this.setState({
+              console: `${this.state.currentPlayer} Wins!`
+          });
+      }
+
+      if ([currentBoard[0][2], currentBoard[1][1], currentBoard[2][0]].every((player) => player === this.state.currentPlayer)) {
+          this.setState({
+              console: `${this.state.currentPlayer} Wins!`
+          });
+      }
+  }
+
+  resetGame() {
+    this.setState({
+        currentGameInputs: [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]],
+        console: "",
+        currentPlayer: "X",
+        previousPlayer: "O",
+    })
   }
 
   render() {
@@ -67,10 +144,16 @@ export default class Game extends React.Component {
                         <div className={"position22"} onClick={ () => {this.onClick(2, 2)}}>{this.state.currentGameInputs[2][2]}</div>
                     </div>
                     <div>
-                        {`Current Turn ${this.state.currentTurn}`}
+                        {`Current Turn: ${this.state.currentPlayer}`}
                     </div>
                     <div>
                         {this.state.console}
+                    </div>
+                    <div>
+                        {this.state.console.includes("Wins") ?
+                            <div  onClick={ () => {this.resetGame()}}><button>Reset Game</button></div>
+                            : <div/>
+                        }
                     </div>
                 </div>
             </div>
