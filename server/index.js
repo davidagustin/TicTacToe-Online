58e4af394811d205adb5f2b53ae9b2e788e87b3a
David Agustin <davidsyagustin@gmail.com>
2019-01-28 21:31:51 -0800

    Change express.listen to app.listen

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const controller = require('./controller.js');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 const PORT = 3000;
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('subscribeToTimer', (interval) => {
+//     console.log('client is subscribing to timer with interval ', interval);
+//   });
+//
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
 
 app.use(express.static(path.join(__dirname, './../client/dist')));
 app.use(bodyParser.json());
@@ -46,6 +46,6 @@ app.post('/mvp', (req, res) => {
   })
 });
 
-http.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('MVP is now listening on port ', PORT)
 });
